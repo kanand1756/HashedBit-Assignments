f5ad9d1e828fc6b56aad174f3e8c01f9a7676756
Krishna Anand <63855692+klm3011@users.noreply.github.com>
2024-05-09 20:07:33 +0530

    Update and rename Java Assignment-002 to Java Assignment-002.js

--- Java Assignment-002.js
+++ Java Assignment-002.js
@@ -1,4 +1,4 @@
-1. Write code to display from 1 to 100 but just even numbers.
+//1. Write code to display from 1 to 100 but just even numbers.
 
 const number=()=>{
   for (let i = 2; i <= 100; i += 2) {
@@ -11,7 +11,7 @@ number();
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-2. Write a function to calculate which can add, subtract, multiply and divide two numbers. Use switch inside function.
+//2. Write a function to calculate which can add, subtract, multiply and divide two numbers. Use switch inside function.
 
 const calculate = (num1, num2, operator) => {
   let result;
@@ -43,7 +43,7 @@ console.log(a);
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-3. You need to write a function findTax() that takes a number type variable as an argument which is the salary of a person. You need to calculate the tax amount on his salary by using the Js switch control flow. The tax rates are given below.
+//3. You need to write a function findTax() that takes a number type variable as an argument which is the salary of a person. You need to calculate the tax amount on his salary by using the Js switch control flow. The tax rates are given below.
 
 if 500000 >= salary > 0 then 0% tax on the entire salary.
 If 1000000 >= salary > 500000 then 10% tax on the entire salary.
@@ -76,7 +76,7 @@ console.log(findTax(1200000)); // Output: 240000 (20% of 1200000)
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////
 
-4. Write a function to perform this. You are given two numbers n1 and n2. You need to find the sum of the products of their corresponding digits. So, for a number n1= 6 and n2 = 34, you'll do (6*4)+(0*3) = 24.
+//4. Write a function to perform this. You are given two numbers n1 and n2. You need to find the sum of the products of their corresponding digits. So, for a number n1= 6 and n2 = 34, you'll do (6*4)+(0*3) = 24.
 
 
 function sumOfProductDigits(n1, n2) {
